2618ec8e149fba6d4f1e5005f7a34e7e5ba04d94
Roman-Robert <116169632+Roman-Robert@users.noreply.github.com>
2022-12-20 20:31:37 +0200

    Initial commit

---
+++ src/main/java/SortingClass.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class SortingClass {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        System.out.println("Введите последовательность целых чисел");
+        for (int i = 0; i < 10; i++) {
+            while (scanner.hasNextInt()) arrayList.add(i, scanner.nextInt());
+        }
+        Collections.sort(arrayList);
+        System.out.println(arrayList);
+    }
+}
