8ae2530acbad4bfdd83538506ebec14ac0fe5394
Roman Robert <robert.roman.alexeevich@gmail.com>
2022-12-26 17:56:39 +0300

    add some classes

--- src/main/java/sortingApp/InputClass.java
+++ src/main/java/sortingApp/InputClass.java
@@ -1,17 +1,18 @@
+package sortingApp;
+
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
-public class SortingClass {
+public class InputClass {
 
-    public static void main(String[] args) {
+    public static ArrayList<Integer> inputArray() {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Integer> arrayList = new ArrayList<>();
         System.out.println("Введите последовательность целых чисел");
         for (int i = 0; i < 10; i++) {
             if (scanner.hasNextInt()) arrayList.add(i, scanner.nextInt());
+            else break;
         }
-        Collections.sort(arrayList);
-        System.out.println(arrayList);
+        return arrayList;
     }
 }
---
+++ src/main/java/sortingApp/MainClass.java
@@ -0,0 +1,8 @@
+package sortingApp;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        System.out.println(SortingClass.sortingArray(InputClass.inputArray()));
+    }
+}
