8af4b10db5eae0533e3d10d08d9806c26491b663
Roman Robert <robert.roman.alexeevich@gmail.com>
2022-12-26 17:05:20 +0300

    change statement while to if

--- src/main/java/SortingClass.java
+++ src/main/java/SortingClass.java
@@ -9,7 +9,7 @@ public class SortingClass {
         ArrayList<Integer> arrayList = new ArrayList<>();
         System.out.println("Введите последовательность целых чисел");
         for (int i = 0; i < 10; i++) {
-            while (scanner.hasNextInt()) arrayList.add(i, scanner.nextInt());
+            if (scanner.hasNextInt()) arrayList.add(i, scanner.nextInt());
         }
         Collections.sort(arrayList);
         System.out.println(arrayList);
