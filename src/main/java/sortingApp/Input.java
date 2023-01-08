67916752654546d784e83d916a9ed097b9421fca
Roman-Robert <116169632+Roman-Robert@users.noreply.github.com>
2023-01-08 14:11:14 +0200

    add javadoc for methods

--- src/main/java/sortingApp/Input.java
+++ src/main/java/sortingApp/Input.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Input {
-
+    /**
+     * The inputArray method accepts integer values from console.
+     * The method accepts no more than 10 integer values (or up to the first not-integer value received).
+     * @return As a result, method returns ArrayList with entered integer values (in order of input).
+     */
     public static ArrayList<Integer> inputArray() {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Integer> arrayList = new ArrayList<>();
@@ -12,7 +16,7 @@ public class Input {
         for (int i = 0; i < 10; i++) {
             if (scanner.hasNextInt()) arrayList.add(scanner.nextInt());
             else {
-                System.out.println("You entered not integer value. Result:");
+                System.out.println("Result:");
                 break;
             }
         }
--- src/main/java/sortingApp/Sorting.java
+++ src/main/java/sortingApp/Sorting.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Sorting {
-
+    /**
+     * The sortingArray method takes ArrayList with integer values and sorts it in ascending order.
+     * @return As a result, method returns sorted ArrayList with entered integer values (in ascending order).
+     * @throws IllegalArgumentException can be thrown.
+     */
     public static ArrayList<Integer> sortingArray(ArrayList<Integer> arrayList) throws IllegalArgumentException {
         if (arrayList.size() <= 10) Collections.sort(arrayList);
         else throw new IllegalArgumentException();
